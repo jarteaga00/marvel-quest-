cb74a0448c9d96cb7b6c6da4c1308695b5d919f9
Andy <a.garcia.atn31@gmail.com>
2022-11-28 18:10:54 -0800

    New gateway key

--- assets/js/thor.js
+++ assets/js/thor.js
@@ -11,13 +11,13 @@ fetch('https://gateway.marvel.com/v1/public/characters?name=Thor&ts=HelloWorld12
 .then(
   function (data){
     //start of thumbnail//
-   //console.log(data.data.results[0].thumbnail.path+'.jpg');
+   console.log(data.data.results[0].thumbnail.path+'.jpg');
    let thumbnail=data.data.results[0].thumbnail.path+'.jpg';
    thor_image.src=thumbnail
    //end of thumbnail//
 
    //start of bio//
-   //console.log(data.data.results[0].description);
+   console.log(data.data.results[0].description);
    let bio=data.data.results[0].description;
    thor_bio.textContent += bio;
    //end of bio//
@@ -25,13 +25,16 @@ fetch('https://gateway.marvel.com/v1/public/characters?name=Thor&ts=HelloWorld12
    //start of comic//
    console.log(data.data.results[0].comics);
    let comic=data.data.results[0].comics.items;
-   fetch('https://gateway.marvel.com/v1/public/comics30090&ts=HelloWorld123&apikey=58cf9dccb6a433565258dd4185258cc5&hash=6a3f4d8c242a80ba92ccd1d638bc7c4f')
+
+
+   fetch('https://gateway.marvel.com/v1/public/comics/30090?ts=HelloWorld123&apikey=58cf9dccb6a433565258dd4185258cc5&hash=6a3f4d8c242a80ba92ccd1d638bc7c4f')
     .then(function (response) {
     return response.json();
     })
     .then(
         function (data){
-        console.log(data)
+        console.log(data.data.items[0])
+        let comic
     })
    }
 );
