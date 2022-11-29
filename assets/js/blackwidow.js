4763f77ceac01a9898f5c1e902c3f4f93a429d83
FrankM89 <114839168+FrankM89@users.noreply.github.com>
2022-11-28 21:31:17 -0800

    Add 3 avengers

--- assets/js/blackwidow.js
+++ assets/js/blackwidow.js
@@ -1,11 +1,11 @@
 let bio = document.getElementById("bio");
 let comics = document.getElementById("comics");
 let movies = document.getElementById("movies");
-let thor_image = document.getElementById("thor_image");
-let thor_bio = document.getElementById("thor_bio");
+let blackwidow_image = document.getElementById("blackwidow_image");
+let blackwidow_bio = document.getElementById("blackwidow_bio");
 
 fetch(
-  "https://gateway.marvel.com/v1/public/characters?name=Thor&ts=HelloWorld123&apikey=58cf9dccb6a433565258dd4185258cc5&hash=6a3f4d8c242a80ba92ccd1d638bc7c4f"
+  "https://gateway.marvel.com/v1/public/characters?name=Black%20Widow&ts=HelloWorld123&apikey=58cf9dccb6a433565258dd4185258cc5&hash=6a3f4d8c242a80ba92ccd1d638bc7c4f"
 )
   .then(function (response) {
     return response.json();
@@ -14,13 +14,13 @@ fetch(
     //start of thumbnail//
     console.log(data.data.results[0].thumbnail.path + ".jpg");
     let thumbnail = data.data.results[0].thumbnail.path + ".jpg";
-    thor_image.src = thumbnail;
+    blackwidow_image.src = thumbnail;
     //end of thumbnail//
 
     //start of bio//
     console.log(data.data.results[0].description);
     let bio = data.data.results[0].description;
-    thor_bio.textContent += bio;
+    blackwidow_bio.textContent += bio;
     //end of bio//
 
     //start of comic//
@@ -34,7 +34,7 @@ fetch(
         return response.json();
       })
       .then(function (data) {
-        console.log(data.data.items[0]);
+        console.log(data.data.items);
         let comic;
       });
   });
