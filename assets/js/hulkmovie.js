401583eb3be60d4bc0cc7faa96725e38019e0e5e
Andy <a.garcia.atn31@gmail.com>
2022-11-30 17:18:56 -0800

    obdm api

---
+++ assets/js/hulkmovie.js
@@ -0,0 +1,9 @@
+fetch('http://www.omdbapi.com/?i=tt3896198&apikey=5d92eb43')
+.then(function (response) {
+    return response.json();
+})
+.then(function (data){
+    console.log(data);
+    document.getElementById('id2');
+
+});
